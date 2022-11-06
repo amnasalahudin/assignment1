f968c39e60fb97fe71681b5879598d6140a0173f
amna0488 <106267541+amnasalahudin@users.noreply.github.com>
2022-11-06 19:34:43 +0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import Login from "./components/login/login"
 import Signup from "./components/signup/signup"
 import AddProduct from "./components/AddProduct/AddProduct"
 import ViewProduct from "./components/ViewProduct/ViewProduct"
-import styled from "styled-components";
+
 
 
 function App() {
@@ -18,29 +18,26 @@ function App() {
       <Router>
       <Switch>
 
-    // if logged in then homepage, otherwise display the login page
       <Route exact path="/">
           {
             user && user._id ? <Homepage setLoginUser={setLoginUser} /> : <Login setLoginUser={setLoginUser}/>
           }
       </Route>
 
-          // for log in
+        
       <Route path="/login">
            <Login setLoginUser={setLoginUser}/>
       </Route>
 
-          // for sign up page
       <Route path="/register">
       <Signup />
       </Route>
 
-          // for create product page
+         
       <Route path="/createProduct">
       <AddProduct />
       </Route>
 
-      // for view product page
       <Route path="/viewProduct">
       <ViewProduct />
       </Route>
